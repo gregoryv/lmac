9e799c0f8bc2208fe0b1a292306ade443cac8623
Gregory Vincic <gregoryvse@gmail.com>
2025-06-21 20:54:37 +0200

    Move update.sh to source/

--- source/update.sh
+++ source/update.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+#
 # NOTE! DO NOT RUN THIS SCRIPT MORE THAN ONCE A DAY
+#
 # See limitation notice on
 # https://regauth.standards.ieee.org/standards-ra-web/pub/view.html
 
-cd source
 if [ $(find mal.csv -mtime +1) ]; then
     echo "update mal.csv"
-    wget -o source/mal.csv https://standards-oui.ieee.org/oui/oui.csv
+    wget -o mal.csv https://standards-oui.ieee.org/oui/oui.csv
 fi
 
 if [ $(find mam.csv -mtime +1) ]; then
     echo "update mam.csv"
-    wget -o source/mam.csv https://standards-oui.ieee.org/oui28/mam.csv
+    wget -o mam.csv https://standards-oui.ieee.org/oui28/mam.csv
 fi
 
 if [ $(find mas.csv -mtime +1) ]; then
     echo "update mas.csv"
-    wget -o source/mas.csv https://standards-oui.ieee.org/oui36/oui36.csv
+    wget -o mas.csv https://standards-oui.ieee.org/oui36/oui36.csv
 fi
 
